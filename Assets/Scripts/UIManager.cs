3f0f0d90369e3b8f7ceaff8e202b3d6529dea459
Kathiravan Thirumurthy <tkathiravan1976@gmail.com>
2023-06-22 17:36:54 +0530

    UI done uptoLevel Loading

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    [SerializeField]
+    private GameObject optioncanvas; // Reference to the canvas you want to toggle
+    [SerializeField]
+    private GameObject controlcanvas; // Reference to the canvas you want to toggle
+    [SerializeField]
+    private GameObject audiocanvas; // Reference to the canvas you want to toggle
+
+
+
+    // Method to toggle the canvas on or off
+    public void ToggleOptionsCanvas()
+    {
+        
+        optioncanvas.SetActive(true);
+       // canvas.enabled = !canvas.enabled;
+    }
+    public void ToggleControlCanvas()
+    {
+        optioncanvas.SetActive(false);
+        controlcanvas.SetActive(true);
+    }
+    public void ToggleAudioCanvas()
+    {
+        optioncanvas.SetActive(false);
+        audiocanvas.SetActive(true);
+    }
+    public void BackToMenu()
+    {
+        
+        optioncanvas.SetActive(false);
+    }
+    public void BackToOptions()
+    {
+       
+        controlcanvas.SetActive(false);
+        audiocanvas.SetActive(false);
+        optioncanvas.SetActive(true);
+    }
+    public void exitApp()
+    {
+        Application.Quit();
+    }
+    public void startLevel()
+    {
+        // optioncanvas.SetActive(false);
+        SceneManager.LoadScene("LevelScreen");
+    }
+}
